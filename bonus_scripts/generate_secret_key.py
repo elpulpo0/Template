f1b27c7cfba1cc63f7d91fae0a48f4a718339ed7
El Pulpo <151920498+elpulpo0@users.noreply.github.com>
2025-07-27 12:40:00 +0200

    Update generate_secret_key.py

--- bonus_scripts/generate_secret_key.py
+++ bonus_scripts/generate_secret_key.py
@@ -6,11 +6,7 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from auth.utils.logger_config import configure_logger
-
-logger = configure_logger()
-
 secret_bytes = os.urandom(32)
 secret_key = base64.urlsafe_b64encode(secret_bytes).rstrip(b"=").decode("utf-8")
 
-logger.info(f"Your generated secret key : {secret_key}")
+print(f"Your generated secret key : {secret_key}")
